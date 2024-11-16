ead1fbae1772df46ffec5a2f6d76bc1928da3231
dayane112 <71454308+dayane112@users.noreply.github.com>
2024-11-15 23:34:42 -0500

    added useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,50 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weather, setWeather] = useState(null);
+  const [lat, setLat] = useState(null);
+  const [long, setLong] = useState(null);
+  const [load, setload] = useState(true)
+
+
+  const apiKey = "eb889a65b1d1b573579883315f022ec8";
+
+  useEffect(() => {
+
+
+    async function FetchWeather(latitude, longitude) {
+
+      const limit = 1;
+
+      try {
+        const res = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=${limit}&appid=${apiKey}`);
+
+        const data = await res.json();
+        setWeather(data);
+      } catch (err) {
+        console.error(err)
+      } finally {
+        setload(false)
+      }
+
+    function location() {
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) =>{
+
+        })
+      }
+    }
+
+      
+    }
+
+    console.log(FetchWeather())
+  }, []);
+
+
+
 
   return (
     <>
